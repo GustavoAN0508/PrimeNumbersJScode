c564fd61652752e0f40c5a124933e1695fa4f539
GustavoAN0508 <163844692+GustavoAN0508@users.noreply.github.com>
2024-05-27 15:06:30 -0300

    Add files via upload
    That's the code! Basically, he first tells you if the number in question, which is 10, a prime number or not and afterwards he tells you about the other prime numbers before him. In that case, 2, 3, 5 and 7.

---
+++ Javascript/APS1_funcao.js
@@ -0,0 +1,43 @@
+/**
+  Trabalho da disciplina de Programação em JavaScript
+  Turma:963R
+  Aluno:Luiz Gustavo Arruda Nascimento
+  Matricula:2024100924
+*/
+
+function isPrime(num){
+  if(num<2){
+    return false;
+  }
+  for(let i=num-1; i>=2; i--){
+      let resto = num % i
+      if (resto == 0){
+        return false; 
+      }
+  }
+  return true;
+}
+
+function findLargestPrimes(num){
+  const primos = [];
+  if(num>=2){
+    primos.push(2);
+  }
+  for(let i=3; i <= num; i+=2){
+    if (isPrime(i) == true){
+      primos.push(i);   
+    }
+  }
+  return primos;
+}
+
+let  num = parseInt(prompt("É numero primo?"));
+let primo = isPrime(num);
+if (primo == true){
+  alert(num + " é numero primo.");
+}
+else{
+  alert(num + " não é numero primo.");
+}
+
+alert("Os números primos correspondestes são: " + findLargestPrimes(num))
